fcc3d869e33a9f47773da7f9bfe74918db886b2d
Spratch <contact@josephclenet.fr>
2024-12-01 23:12:10 +0100

    feat(chart): start at the bottom of the chart

--- components/appUi/main.tsx
+++ components/appUi/main.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import EntityDetails from "./entityDetails"
+import EntityDetails from "./entityDetails";
 
 import Chart from "../chart/chart";
 import { CurrentType } from "../../types/current";
@@ -8,42 +8,60 @@ import { EventType } from "../../types/event";
 import { RepublicType } from "../../types/republic";
 import { TooltipProvider } from "../utils/tooltipContext";
 import { useDetailsContext } from "../utils/detailsContext";
+import { useEffect, useRef, useState } from "react";
 
 type Props = {
-    republics: RepublicType[];
-    currents: CurrentType[];
-    events: EventType[];
-    eventsVisibility: boolean;
-    referenceSize: number;
-}
+  republics: RepublicType[];
+  currents: CurrentType[];
+  events: EventType[];
+  eventsVisibility: boolean;
+  referenceSize: number;
+};
 
+export default function Main({
+  republics,
+  currents,
+  events,
+  eventsVisibility,
+  referenceSize
+}: Props) {
+  const { detailsContent } = useDetailsContext();
+  const selectedEntity = detailsContent?.entity;
 
-export default function Main({republics, currents, events, eventsVisibility, referenceSize}: Props) {
-    const { detailsContent } = useDetailsContext();
-    const selectedEntity = detailsContent?.entity;
+  // Set the scroll position to the bottom on first render
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [hasScrolled, setHasScrolled] = useState(false);
 
-    return (
-        <main 
-            className="relative w-full max-w-screen-3xl mx-auto h-[calc(100dvh-6.5rem)]"
-        >
-            <div className="w-full h-full overflow-y-scroll no-scrollbar overscroll-none">
-                {/* Chart */}
-                {republics && currents && events && (
-                    <TooltipProvider>
-                        <Chart 
-                            republics={republics} 
-                            currents={currents} 
-                            events={events} 
-                            eventsVisibility={eventsVisibility}
-                            referenceSize={referenceSize}
-                        />
-                    </TooltipProvider>
-                )}
-                
-                {selectedEntity && (
-                    <EntityDetails/>
-                )}
-            </div>
-        </main>
-    )
+  useEffect(() => {
+    if (!hasScrolled && scrollRef.current) {
+      if (scrollRef.current.scrollHeight > scrollRef.current.clientHeight) {
+        scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+        setHasScrolled(true);
+      }
+    }
+  }, [hasScrolled, scrollRef, republics]);
+
+  return (
+    <main className="relative w-full max-w-screen-3xl mx-auto h-[calc(100dvh-6.5rem)]">
+      <div
+        ref={scrollRef}
+        className="w-full h-full overflow-y-scroll no-scrollbar overscroll-none"
+      >
+        {/* Chart */}
+        {republics && currents && events && (
+          <TooltipProvider>
+            <Chart
+              republics={republics}
+              currents={currents}
+              events={events}
+              eventsVisibility={eventsVisibility}
+              referenceSize={referenceSize}
+            />
+          </TooltipProvider>
+        )}
+
+        {selectedEntity && <EntityDetails />}
+      </div>
+    </main>
+  );
 }
