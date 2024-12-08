a4388420f8bbd5c917b8cf6c4e1053c9abf57c7f
Spratch <contact@josephclenet.fr>
2024-12-08 21:16:37 +0100

    feat(infos): gradients, reformulations

--- components/appUi/iconButton.tsx
+++ components/appUi/iconButton.tsx
@@ -6,12 +6,14 @@ type Props = {
 
 export default function IconButton({ Icon, label, onClick }: Props) {
   return (
-    <button
-      aria-label={label}
-      className="size-9 bg-black/5 hover:bg-black/10 rounded-full flex items-center justify-center cursor-pointer text-black/50 hover:text-black transition-all flex-shrink-0"
-      onClick={onClick}
-    >
-      <Icon className="size-4" />
-    </button>
+    <div className="bg-white rounded-full">
+      <button
+        aria-label={label}
+        className="size-9 bg-black/5 hover:bg-black/10 rounded-full flex items-center justify-center cursor-pointer text-black/50 hover:text-black transition-all flex-shrink-0"
+        onClick={onClick}
+      >
+        <Icon className="size-4" />
+      </button>
+    </div>
   );
 }
--- components/appUi/infosModal.tsx
+++ components/appUi/infosModal.tsx
@@ -65,15 +65,17 @@ export default function InfosModal({ setInfosVisibility }: Props) {
         className="bg-white h-full w-full rounded-2xl shadow-lg max-w-screen-xl mx-auto relative overflow-x-scroll"
         role="popover"
       >
-        <div className="sticky top-0 flex justify-end w-full p-4 gap-3">
-          <Link
-            className="size-9 bg-black/5 hover:bg-black/10 rounded-full flex items-center justify-center cursor-pointer text-black/50 hover:text-black transition-all"
-            href="#"
-            target="_blank"
-            title="Voir le code source"
-          >
-            <GitHubLogoIcon className="size-5" />
-          </Link>
+        <div className="sticky top-0 flex justify-end w-full p-4 gap-3 bg-gradient-to-b from-white z-50">
+          <div className="bg-white rounded-full">
+            <Link
+              className="size-9 bg-black/5 hover:bg-black/10 rounded-full flex items-center justify-center cursor-pointer text-black/50 hover:text-black transition-all"
+              href="#"
+              target="_blank"
+              title="Voir le code source"
+            >
+              <GitHubLogoIcon className="size-5" />
+            </Link>
+          </div>
 
           <IconButton
             Icon={Cross1Icon}
@@ -86,19 +88,18 @@ export default function InfosModal({ setInfosVisibility }: Props) {
           <h1 className="col-span-12 md:col-span-7 text-3xl md:text-5xl">
             <span className="opacity-40">Entre crises et revendications</span>
             <br />
-            Les évolutions des courants politiques au sein de l’Assemblée
+            Les évolutions des courants politiques au sein de l&rsquo;Assemblée
             Nationale*
           </h1>
           <div className="col-span-12 md:col-span-5 flex flex-col items-start text-xl md:text-2xl">
             <p className="opacity-75">
               Ce site est une représentation visuelle des résultats des
-              élections législatives françaises depuis la première assemblée
-              nationale législative en 1791.
+              élections législatives françaises depuis la première en 1791.
             </p>
           </div>
         </section>
 
-        <section className="grid grid-cols-12 gap-x-6 px-5 sm:px-10 pb-8">
+        <section className="grid grid-cols-12 gap-x-6 px-5 sm:px-10">
           <article className="col-span-12 sm:col-span-7 py-8 border-b border-black max-w-prose flex flex-col gap-4">
             <p>
               Cette visualisation a été réalisée à partir de données de sources
@@ -112,48 +113,48 @@ export default function InfosModal({ setInfosVisibility }: Props) {
                 Wikipédia
               </InfoLink>
               . Plus les données sont anciennes, moins elles sont précises. En
-              effet, les concepts de parti politique ou de groupe parlementaire
-              n&apos;ont pas toujours existé et les députés ont pu changer de
-              parti en cours de mandat. Les données sont donc à prendre avec
-              précaution.
+              effet, les concepts modernes de partis politiques ou de groupes
+              parlementaires n&rsquo;existaient pas toujours. Les députés
+              étaient alors étiquetés en fonction de leurs votes, une fois élus.
             </p>
             <p>
-              De plus, il a fallut trancher de manière arbitraire sur
-              l&apos;appartenance de chaque parti ou député à un courant
+              De plus, il a fallu trancher de manière arbitraire sur
+              l&rsquo;appartenance de chaque parti ou député à un courant
               politique. Les nuances et les évolutions des partis politiques ne
-              sont d&apos;ailleurs pas forcément prises en compte. Ajoutons à
+              sont d&rsquo;ailleurs pas forcément prises en compte. Ajoutons à
               cela que les familles politiques telles que la droite ou la gauche
               sont des notions qui, au fil du temps, ont pu se déplacer.
-              L&apos;extrême gauche du début de la III<sup>e</sup> République
-              pourrait correspondre aujourd&apos;hui à un positionnement
-              centriste. Pour plus de clarté, j&apos;ai décidé de classer les
-              partis dans des repères plutôt actuels.
+              L&rsquo;extrême gauche du début de la III<sup>e</sup> République
+              pourrait correspondre aujourd&rsquo;hui à un positionnement
+              centriste. Pour plus de clarté, j&rsquo;ai décidé de classer les
+              partis à partir de repères contemporains.
             </p>
             <p>
-              Enfin, j&apos;ai souhaité représenter les accords éléctoraux entre
-              partis par des coalitions. Celles-ci permettent de mieux
-              comprendre les rapports de force qui s&apos;opèrent. Elles
-              n&apos;ont pas toujours les mêmes modalités, et plus les éléctions
-              sont anciennes, moins les informations sont accessibles.
+              Enfin, j&rsquo;ai souhaité représenter les accords électoraux
+              entre partis tels que les coalitions. Celles-ci permettent de
+              mieux comprendre les rapports de force qui s&rsquo;opèrent. Elles
+              n&rsquo;ont pas toujours les mêmes modalités, et les informations
+              manquent parfois pour les élections les plus anciennes.
             </p>
           </article>
           <article className="col-span-12 sm:col-span-5 py-8 border-b border-black max-w-prose flex flex-col gap-4">
             <p>
               * Les assemblées ont prit différentes formes et différents noms,
-              les modes de scrutin ont évolué et l&apos;Histoire a avancé en
+              les modes de scrutin ont évolué et l&rsquo;Histoire a avancé en
               rendant chaque élection unique par les enjeux politiques et
-              sociaux de l&apos;époque. Ce site propose une vision synthétique,
+              sociaux de l&rsquo;époque. Ce site propose une vision synthétique,
               et donc incomplète quant aux contextes entourant chaque élection.
             </p>
             <p>
               Cependant, des événements marquants sont placés en marge du
-              graphique principal pour contextualiser davantage les résultats
-              dans les rapports sociaux et (géo)politiques de l&apos;époque.
+              graphique principal pour contextualiser davantage les résultats.
+              Ces choix ne prétendent pas à l&rsquo;exhaustivité et peuvent
+              gagner en pertinence. Vos retours sont les bienvenus.
             </p>
           </article>
-          <article className="col-span-12 sm:col-span-7 py-8 max-w-prose flex flex-col gap-4">
+          <article className="col-span-12 sm:col-span-7 py-8 sm:pb-0  max-w-prose flex flex-col gap-4 border-b border-black sm:border-none">
             <p>
-              Ce graphique est inspirée du superbe travail de{" "}
+              Ce graphique est inspiré du superbe travail de{" "}
               <InfoLink href="https://mastodon.social/@tomfevrier/112751171765233201">
                 Tom Février et Marie Patino
               </InfoLink>{" "}
@@ -161,29 +162,30 @@ export default function InfosModal({ setInfosVisibility }: Props) {
               <InfoLink href="https://www.bloomberg.com/graphics/2024-french-election-results/">
                 cet article de Bloomberg
               </InfoLink>
-              . En le découvrant, j&apos;aurais aimé pouvoir l&apos;étendre
-              jusqu&apos;à la révolution de 1789. C&apos;est donc ce que
-              j&apos;ai fait. Je l&apos;ai fait pour le plaisir, pour apprendre
-              à créer des visualisations de données, et pour mieux comprendre
-              l&apos;histoire politique de la France.
+              . En le découvrant, j&rsquo;aurais aimé pouvoir l&rsquo;étendre
+              jusqu&rsquo;à la révolution de 1789. C&rsquo;est donc ce que
+              j&rsquo;ai fait. Je l&rsquo;ai fait pour le plaisir, pour
+              apprendre à créer des visualisations de données, et pour mieux
+              comprendre l&rsquo;histoire politique de la France.
             </p>
             <p>
-              J&apos;espère que vous prendrez autant de plaisir à le consulter,
-              qu&apos;il vous apportera connaissances ou mises en perspectives.
+              J&rsquo;espère que vous prendrez autant de plaisir à le consulter,
+              qu&rsquo;il vous apportera connaissances ou mises en perspectives.
               Si vous y rencontrez des erreurs, des imprécisions, des oublis, ou
               si vous avez des suggestions,{" "}
               <a
                 href="mailto:"
                 className="text-blue-500 hover:opacity-70 transition-opacity"
               >
-                n&apos;hésitez pas à me contacter
+                n&rsquo;hésitez pas à me contacter
               </a>
-              . Je n&apos;ai pas été totalement méthodique dans mon approche, et
-              j&apos;ai pu commettre des erreurs. Je suis ouvert à toute
-              critique constructive, tant sur le contenu que sur le code.
+              . Je ne suis ni historien, ni expert en politique, et c&rsquo;est
+              mon premier projet de visualisation de données. Je suis donc
+              ouvert à toute critique constructive, tant sur le contenu que sur
+              le code.
             </p>
           </article>
-          <article className="col-span-12 sm:col-span-5 py-8 max-w-prose flex flex-col gap-4">
+          <article className="col-span-12 sm:col-span-5 pt-8 max-w-prose flex flex-col gap-4">
             <p>
               Je remercie également{" "}
               <span className="text-blue-500">
@@ -193,6 +195,7 @@ export default function InfosModal({ setInfosVisibility }: Props) {
             </p>
           </article>
         </section>
+        <div className="sticky bottom-0 w-full h-16 bg-gradient-to-t from-white"></div>
       </div>
     </div>
   );
